44995c4274a3aab738f67ec61f6e6b36a6d96b09
Kevin <kevincchavez@gmail.com>
2024-11-26 15:49:04 +0800

    update css

--- script.js
+++ script.js
@@ -10,31 +10,26 @@ window.addEventListener('resize', adjustLogoWidth);
 // Function to adjust the logo width based on the current window width
 function adjustLogoWidth() {
     const logo = document.querySelector('.logo');
+    const nav_expand_section = document.querySelector('.nav-expand-section');
+
+    
+    
     const windowWidth = window.innerWidth;
+
+    nav_expand_section.style.width = `${windowWidth}px`;
 
     if (windowWidth <= 767) {
         
-
-        // Calculate the logo width
-        const logoWidth = windowWidth * 0.1;
-
-        // Set minimum and maximum width (e.g., min 100px, max 300px)
-        const adjustedWidth = Math.min(Math.max(logoWidth, 100), 300);
-
         // Apply the width to the logo
-        logo.style.width = `${windowWidth - 155}px`;
+        logo.style.width = `${windowWidth - 140}px`;
+        nav_expand_section.style.width = `${windowWidth}px`;
         
 
     } else if(windowWidth <= 1023) {
 
-        // Calculate the logo width
-        const logoWidth = windowWidth * 0.1;
-
-        // Set minimum and maximum width (e.g., min 100px, max 300px)
-        const adjustedWidth = Math.min(Math.max(logoWidth, 100), 300);
-
         // Apply the width to the logo
         logo.style.width = `${windowWidth - 312}px`;
+        nav_expand_section.style.width = `${windowWidth}px`;
         
     } else {
         logo.style.width = '';
